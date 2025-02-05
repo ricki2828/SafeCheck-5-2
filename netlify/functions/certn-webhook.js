f5b5b1b84da2117925434875b2daacec4738160a
ricki2828 <ricki.taiaroa@gmail.com>
2025-02-05 22:36:36 +0800

    10:36pm

--- netlify/functions/certn-webhook.js
+++ netlify/functions/certn-webhook.js
@@ -1,23 +1,10 @@
 import { Handler } from '@netlify/functions';
 
 export const handler: Handler = async (event) => {
-  // Log everything
-  console.log('DEBUG - Full event:', {
-    method: event.httpMethod,
-    path: event.path,
-    query: event.queryStringParameters,
-    headers: event.headers,
-    rawQuery: event.rawQuery
-  });
-
   // For GET requests (webhook verification)
   if (event.httpMethod === 'GET') {
     const challenge = event.queryStringParameters?.challenge;
-    console.log('DEBUG - Challenge value:', challenge);
-
-    // Always log what we're returning
-    console.log('DEBUG - Responding with:', challenge || 'no challenge found');
-
+    
     if (challenge) {
       return {
         statusCode: 200,
@@ -25,15 +12,16 @@ export const handler: Handler = async (event) => {
           'Content-Type': 'text/plain',
           'X-Content-Type-Options': 'nosniff'
         },
-        body: challenge
+        body: challenge  // Return ONLY the challenge string
       };
     }
   }
 
-  // Log error case
-  console.log('DEBUG - Invalid request received');
   return {
     statusCode: 400,
+    headers: {
+      'Content-Type': 'text/plain'
+    },
     body: 'Invalid request'
   };
 };
