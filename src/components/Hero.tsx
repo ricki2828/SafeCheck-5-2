be5e268232a435794bc781fe2445331d63fdb682
ricki2828 <ricki.taiaroa@gmail.com>
2025-02-07 12:07:30 +0800

    testi

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,19 @@
 export default function Hero() {
   return (
     <div className="relative isolate overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+      >
+        <source src="/videos/hero.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-dark to-dark/50" />
+
       <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
-          <div className="absolute inset-0 -z-10">
-            <video
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="h-full w-full object-cover"
-            >
-              <source src="/videos/hero.mp4" type="video/mp4" />
-            </video>
-            <div className="absolute inset-0 bg-gradient-to-t from-dark to-dark/50" />
-          </div>
-
           <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
             The fastest official criminal check in Canada
           </h1>
